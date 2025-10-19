efa95a2700afec640d55abec610eb4fdb61dece0
oak_cassia <gnlckswjd1@gmail.com>
2025-10-20 00:08:52 +0900

    feat: Ollama로 AIAgent 사용

--- Agentic-Design-Pattern-With-MAF/Program.cs
+++ Agentic-Design-Pattern-With-MAF/Program.cs
@@ -2,12 +2,14 @@
 using Azure.AI.OpenAI;
 using Azure.Identity;
 using Microsoft.Agents.AI;
+using OllamaSharp;
 using OpenAI;
 
-AIAgent agent = new AzureOpenAIClient(
-        new Uri("https://your-resource.openai.azure.com/"),
-        new AzureCliCredential())
-    .GetChatClient("gpt-4o-mini")
-    .CreateAIAgent(instructions: "You are good at telling jokes.");
+var chatClient = new OllamaApiClient(new Uri("http://localhost:11434"), "phi4-mini-reasoning");
+
+AIAgent agent = new ChatClientAgent(
+    chatClient,
+    instructions: "You are good at telling jokes",
+    name: "Joker");
 
 Console.WriteLine(await agent.RunAsync("Tell me a joke about a pirate."));
