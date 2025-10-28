434b1f7653af0792ccb4cff878b5c02c4f89cfc3
oak_cassia <gnlckswjd1@gmail.com>
2025-10-28 22:43:38 +0900

    feat: use function tools

--- Agentic-Design-Pattern-With-MAF/Program.cs
+++ Agentic-Design-Pattern-With-MAF/Program.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.Agents.AI;
+﻿using System.ComponentModel;
+using Microsoft.Agents.AI;
 using OllamaSharp;
+using Microsoft.Extensions.AI;
 
-var chatClient = new OllamaApiClient(new Uri("http://localhost:11434"), "phi4-mini-reasoning");
+var chatClient = new OllamaApiClient(new Uri("http://localhost:11434"), "phi4-mini");
 
 AIAgent agent = new ChatClientAgent(
     chatClient,
-    instructions: "You are good at telling jokes",
-    name: "Joker");
+    instructions: "You are a helpful assistant",
+    name: "Joker",
+    tools: [AIFunctionFactory.Create(GetWeather)]);
 
-Console.WriteLine(await agent.RunAsync("Tell me a joke about a pirate."));
+Console.WriteLine(await agent.RunAsync("What is the weather like in Amsterdam?"));
+
+// AgentThread thread = agent.GetNewThread();
+// Console.WriteLine(await agent.RunAsync("Tell me a joke about a pirate.", thread));
+// Console.WriteLine(await agent.RunAsync("Now add some emojis to the joke and tell it in the voice of a pirate's parrot.", thread));
+
+
+[Description("Get the weather for a given location.")]
+static string GetWeather([Description("The location to get the weather for.")] string location)
+    => $"The weather in {location} is cloudy with a high of 15°C.";
