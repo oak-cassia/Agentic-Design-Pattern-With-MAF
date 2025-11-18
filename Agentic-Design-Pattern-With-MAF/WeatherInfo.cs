a7a6ca309a3b83c863976ffa7cedd543da141e53
oak_cassia <gnlckswjd1@gmail.com>
2025-11-18 23:09:45 +0900

    feat: add WeatherInfo DTO for weather data representation

---
+++ Agentic-Design-Pattern-With-MAF/WeatherInfo.cs
@@ -0,0 +1,8 @@
+namespace CategorizationAgent.DTOs;
+
+public class WeatherInfo
+{
+    public string? Location { get; set; }
+    public double Temperature { get; set; }
+    public double Humidity { get; set; }
+}
